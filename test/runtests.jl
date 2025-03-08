dad8a47fe6adc11481f62e6951ea757315a2c1c5
Thomas Dubos <thomas.dubos@polytechnique.edu>
2025-03-08 17:57:27 +0100

    check gradients

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using JaxCall: compile
-using Enzyme: gradient, Reverse, Const
+using Enzyme: Enzyme, gradient, Reverse, Const
+Enzyme.Compiler.VERBOSE_ERRORS[] = true
 
 using Test
 using InteractiveUtils
@@ -13,26 +14,22 @@ L2(x) = sum(x.^2)
 
 function main1()
     fun(b,c) = b*c
-    loss(b,c) = L2(cfun(b,c)-a)
-
-    # L(b,c) = <bc-a | bc-a>
-    # dL = 2 < b⋅c-a | b.dc + db.c >
-    #    = 2 < b'⋅(bc-a) | dc >  2 < (b⋅c-a)⋅c' | db >
-    # dL/db = (b⋅c-a)⋅c'
-    # dL/dc = b'⋅(b⋅c-a)
+    loss(b,c) = L2(fun(b,c)-a)
+    closs(b,c) = L2(cfun(b,c)-a)
 
     M, N, P = 10,20,40
     b = randn(M,N)
     c = randn(N,P)
     a = randn(M,P)
+
     cfun = compile(fun, b, c ; verbose=true)
     @code_warntype cfun(b,c)
-    @info loss(b,c)
+
     grad = gradient(Reverse, Const(loss), b, c)
-    @info typeof(grad)
-    d = b*c-a
-#    @test dLdb ≈ 2d*c'
-#    @test dLdc ≈ 2b'*d
+    cgrad = gradient(Reverse, Const(closs), b, c)
+    @test fun(b,c) ≈ cfun(b,c)
+    @test grad[1] ≈ cgrad[1]
+    @test grad[2] ≈ cgrad[2]
 end
 
 main1()
