64a14ce1de6c556d2481a59f8a45dabed7a57a8e
Thomas Dubos <thomas.dubos@polytechnique.edu>
2025-03-08 17:25:22 +0100

    Repair test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,28 +1,29 @@
-import JaxCall
-import Enzyme
+using JaxCall: compile
+using Enzyme: gradient, Reverse, Const
 
 using Test
 using InteractiveUtils
 
-fun(b,c) = b*c
+L2(x) = sum(x.^2)
 
 function main1()
-    M, N, P = 10,20,40
-    b = randn(M,N)
-    c = randn(N,P)
-    cfun = JaxCall.compile(fun, b, c ; verbose=true)
+    fun(b,c) = b*c
+    loss(b,c) = L2(cfun(b,c)-a)
 
-    @code_warntype cfun(b,c)
-
-    a = randn(M,P)
-    loss(b,c) = sum(abs2, cfun(b,c)-a)
     # L(b,c) = <bc-a | bc-a>
     # dL = 2 < b⋅c-a | b.dc + db.c >
     #    = 2 < b'⋅(bc-a) | dc >  2 < (b⋅c-a)⋅c' | db >
     # dL/db = (b⋅c-a)⋅c'
     # dL/dc = b'⋅(b⋅c-a)
+
+    M, N, P = 10,20,40
+    b = randn(M,N)
+    c = randn(N,P)
+    a = randn(M,P)
+    cfun = compile(fun, b, c ; verbose=true)
+    @code_warntype cfun(b,c)
     @info loss(b,c)
-    dLdb, dLdc = Enzyme.gradient(Reverse, Const(loss), b, c)
+    dLdb, dLdc = gradient(Reverse, Const(loss), b, c)
     d = b*c-a
     @test dLdb ≈ 2d*c'
     @test dLdc ≈ 2b'*d
